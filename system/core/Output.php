7bc7cc96293ad4917089dabadc1b330180feda3c
Ajay Singh <bhandariajay372@gmail.com>
2022-04-14 15:48:19 +0530

    change error

--- system/core/Output.php
+++ system/core/Output.php
@@ -451,11 +451,14 @@ class CI_Output {
 
 		$elapsed = $BM->elapsed_time('total_execution_time_start', 'total_execution_time_end');
 
-		if ($this->parse_exec_vars === TRUE)
-		{
-			$memory	= round(memory_get_usage() / 1024 / 1024, 2).'MB';
-			$output = str_replace(array('{elapsed_time}', '{memory_usage}'), array($elapsed, $memory), $output);
-		}
+		// if ($this->parse_exec_vars === TRUE)
+		// {
+		// 	$memory	= round(memory_get_usage() / 1024 / 1024, 2).'MB';
+		// 	$output = str_replace(array('{elapsed_time}', '{memory_usage}'), array($elapsed, $memory), $output);
+		// }
+         
+
+
 
 		// --------------------------------------------------------------------
 
